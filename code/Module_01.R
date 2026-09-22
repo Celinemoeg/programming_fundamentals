cd8ee8d8c32fcebff9ea0f199003bcc61351e7af
Celine Egsaeter <Celinemoeg@gmail.com>
2026-09-22 11:49:54 +1000

    first comit of new repo

---
+++ code/Module_01.R
@@ -0,0 +1,12 @@
+
+fish_lengths <- c(124, 152, 98, 221, 146)
+coral_spp <- c("Porites", "Acropora", "Montastrea")
+
+
+
+notes <- c("Acropora", 27.5, TRUE)
+
+
+
+notes <- list("Acropora", 27.5, TRUE)
+notes
